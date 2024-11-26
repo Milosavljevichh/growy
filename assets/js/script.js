d61cd9f363026a661b0804ace6c98db0666a6deb
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2024-11-26 20:01:17 +0100

    din ispis dostignuca

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,68 @@ if (navBar){
     }
     navBar.appendChild(ul)
 }
+
+//Achievements
+const achievementContainer = document.querySelector('#achievementContainer')
+const achievements = [
+    {
+        icon:"fa-code-branch",
+        numberVal:128,
+        text:"Ideas",
+        isSolid: true
+    },
+    {
+        icon:"fa-folder-open",
+        numberVal:103,
+        text:"Projects",
+        isSolid: false
+    },
+    {
+        icon:"fa-signal",
+        numberVal:95,
+        text:"Impact",
+        isSolid: true
+    },
+    {
+        icon:"fa-laptop",
+        numberVal:100,
+        text:"Solutions",
+        isSolid: true
+    },
+    {
+        icon:"fa-gem",
+        numberVal:120,
+        text:"Milestones",
+        isSolid: false
+    },
+]
+
+function createAchievement(achievement, index){
+
+    const li = document.createElement('li')
+    li.classList.add(`style${index + 1}`)
+
+    const span = document.createElement('span')
+    span.classList.add(`icon`)
+    achievement.isSolid ? span.classList.add(`solid`) : null
+    span.classList.add(achievement.icon)
+
+    const strong = document.createElement('strong')
+    strong.innerHTML = achievement.numberVal
+
+    const span2 = document.createElement('span')
+    span2.innerHTML = achievement.text
+
+    li.append(span, strong, span2)
+    
+    return li
+}
+
+if (achievementContainer) {
+    const ul = document.createElement('ul')
+    ul.classList.add("statistics")
+    for (let i = 0;i<achievements.length;i++){
+        ul.appendChild(createAchievement(achievements[i], i))
+    }
+    achievementContainer.appendChild(ul)
+}
